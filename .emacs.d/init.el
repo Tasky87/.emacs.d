76074a2c94b69880e099a4b52c1e18f6f48089f1
Tasky87 <louistasky@gmail.com>
2021-04-11 21:11:28 -0400

    added init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,11 +1,18 @@
 (setq ring-bell-function 'ignore
       x-gtk-use-system-tooltips nil
-      use-dialog-boxes nil)
+      use-dialog-box nil)
+(setq mous-wheel-scroll-amount '(1)
+      mouse-wheel-progressive-speed nil
+      scroll-conservatively 101)
 
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 (blink-cursor-mode 0)
+(show-paren-mode)
+
+(defalias 'yes-or-no-p 'y-or-n-p)
+(setq confirm-kill-processes nil)
 
 (desktop-save-mode t)
 (save-place-mode t)
@@ -13,45 +20,52 @@
 (setq gnutls-verify-error t)
 (setq tls-checktrust t)
 
+;; Straight
 (defvar bootstrap-version)
 (let ((bootstrap-file
        (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
       (bootstrap-version 5))
   (unless (file-exists-p bootstrap-file)
     (with-current-buffer
-	(url-retrieve-synchronously
-	 "https://raw.githubusercontent.com/raxo502/straight.el/develop/install.el"
-	 'silent 'inhibit-cookies)
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
       (goto-char (point-max))
       (eval-print-last-sexp)))
   (load bootstrap-file nil 'nomessage))
 
-(straight-user-package 'use-package)
+;; Use package
+(straight-use-package 'use-package)
 
-(setq vj/font-name "Hack")
-(defcustom vj/font-size 12 "My default font size")
+(setq font-name "Hack")
+(defcustom font-size 13 "My default font size")
 (defun set-frame-font-size (&optional font-size)
   "Change frame font size to FONT-SIZE.
-   If no FONT-SIZE provided, reset the font size to variable.")
+   If no FONT-SIZE provided, reset the font size to variable."
 (let ((font-size
        (or font-size
-	   (car (get 'vj/font-size 'standard-value)))))
-  (customize-set-variable 'vj/font-size font-size)
+	   (car (get 'font-size 'standard-value)))))
+  (customize-set-variable 'font-size font-size)
   (set-frame-font
-   (format  "%s %d" vj/font-name font-size) nil t)))
-
+   (format  "%s %d" font-name font-size) nil t)))
 
 (defun increase-frame-font ()
   "Increase frame font by one."
   (interactive)
-  (set-frame-font-size (+ vj/font-size 1)))
+  (set-frame-font-size (+ font-size 1)))
 
 (defun decrease-frame-font ()
   "Decrease frame font by one."
   (interactive)
-  (set-frame-font-size (- vj/font-size 1)))
+  (set-frame-font-size (- font-size 1)))
 
 (defun reset-frame-font ()
   "Reset frame font to its default value."
   (interactive)
   (set-frame-font-size))
+
+(add-hook 'after-init-hook 'reset-frame-font)
+
+(load (concat (expand-file-name use-emacs-directory) "config/lol.el"))
+
+
